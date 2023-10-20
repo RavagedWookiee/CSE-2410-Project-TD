bfeac0a431e5fd270048766f1df55440463a7206
RavagedWookiee <nederhoedc@gmail.com>
2023-10-20 19:27:53 -0400

    Tower Tracking

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -10,7 +10,19 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    //add a reference for each map
+    //for now just three maps with differnt enemy patterns
     public void Easy(string Game) {
+
+        SceneManager.LoadScene(Game);
+    }
+
+    public void Medium(string Game) {
+
+        SceneManager.LoadScene(Game);
+    }
+
+    public void Hard(string Game) {
 
         SceneManager.LoadScene(Game);
     }
